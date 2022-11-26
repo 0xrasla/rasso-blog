04e9f83ffc51bc31845561707ff471a6def50d8e
rasla <programmer.rasla@gmail.com>
2022-11-26 21:14:52 +0530

    added some content

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import prisma from "../lib/prismaclient";
 
 const Home: NextPage = () => {
-  return <div className="main">Hello</div>;
+  return <div className="main">Hello World</div>;
 };
 
 export default Home;
